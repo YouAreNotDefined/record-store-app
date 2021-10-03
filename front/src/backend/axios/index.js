3a9de11a6fe0a9565709a2a0b8c6052a7556123a
root <root@LAPTOP-BBNAIMCU.localdomain>
2021-10-03 10:08:49 +0900

    setting Vue.js

---
+++ front/src/backend/axios/index.js
@@ -0,0 +1,55 @@
+import axios from 'axios'
+
+const APIURL = 'http://localhost:3000/'
+
+const securedAxiosInstance = axios.create({
+  baseURL: APIURL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+const plainAxiosInstance = axios.create({
+  baseURL: APIURL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+securedAxiosInstance.interceptors.request.use(config => {
+  const method = config.method.toUpperCase()
+  if (method !== 'OPTIONS' && method !== 'GET') {
+    config.headers = {
+      ...config.headers,
+      'X-CSRF-TOKEN': localStorage.csrf
+    }
+  }
+
+  return config
+})
+
+securedAxiosInstance.interceptors.response.use(null, error => {
+  if (error.response && error.response.config && error.response.status === 401) {
+    return plainAxiosInstance.post('/refresh', {}, { headers: { 'X-CSRF-TOKEN': localStorage.csrf } })
+      .then(response => {
+        localStorage.csrf = response.data.csrf
+        localStorage.signedIn = true
+
+        const retryConfig = error.response.config
+        retryConfig.headers['X-CSRF-TOKEN'] = localStorage.csrf
+        return plainAxiosInstance.request(retryConfig)
+      }).catch(error => {
+        delete localStorage.csrf
+        delete localStorage.signedIn
+
+        location.replace('/')
+        return Promise.reject(error)
+      })
+  } else {
+    return Promise.reject(error)
+  }
+})
+
+export { securedAxiosInstance, plainAxiosInstance }
--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import './assets/style/tailwind.css'
+import router from './router'
+import VueAxios from 'vue-axios'
+import { securedAxiosInstance, plainAxiosInstance } from './backend/axios'
 
-createApp(App).mount('#app')
+Vue.use(VueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance
+})
+// Vue.use(Router)
+
+const config = {
+  el: '#app',
+  router,
+  securedAxiosInstance,
+  plainAxiosInstance,
+  components: { App },
+  template: '<App/>'
+}
+
+const vm = new Vue()
+
+vm.$mount(config)
